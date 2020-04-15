82812742117fc26b3f9004f07c2df96ea169d6bb
Cuong Tran <cuong@koho.ca>
2020-04-15 18:13:54 -0400

    add ajax-example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { StoreModule } from "@ngrx/store";
-import { counterReducer } from "./counter.reducer";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
-import { MyCounterComponent } from "./my-counter/my-counter.component";
+import { counterReducer } from "./counter.reducer";
+import { ajaxExampleReducer } from "./ajax-example.reducer";
+
+import { AjaxExampleEffect } from "./ajax-example.effects";
 
 import { AppComponent } from "./app.component";
+import { MyCounterComponent } from "./my-counter/my-counter.component";
+import { AjaxExampleComponent } from "./ajax-example/ajax-example.component";
 
 @NgModule({
-  declarations: [AppComponent, MyCounterComponent],
-  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
+  declarations: [AppComponent, MyCounterComponent, AjaxExampleComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    StoreModule.forRoot({
+      count: counterReducer,
+      ajaxExample: ajaxExampleReducer
+    }),
+    StoreDevtoolsModule.instrument({}),
+    EffectsModule.forRoot([AjaxExampleEffect])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
